7c34b30a0c500f413add4c097bfa497c41fb8904
MrMutantus <23027596+MrMutantus@users.noreply.github.com>
2022-06-11 00:05:46 +0200

    initial(project): project initialized

---
+++ src/main/java/de/mrmutantus/luckygames/LuckyGamesApplication.java
@@ -0,0 +1,13 @@
+package de.mrmutantus.luckygames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckyGamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckyGamesApplication.class, args);
+    }
+
+}
